3a24cb683719ca5104fad346324b9916a15d915a
nsakalkale <sakalkalenimish2004@gmail.com>
2024-04-29 00:09:19 +0530

    index

--- server/index.js
+++ server/index.js
@@ -8,15 +8,33 @@ const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 require("dotenv").config();
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 
-const secret = crypto.randomBytes(32).toString("hex");
-app.use(express.json());
+// CORS middleware function
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
+// Handler function for main endpoint
+const handler = (req, res) => {
+  const d = new Date();
+  res.end(d.toString());
+};
+
+// Connect to MongoDB
 const mongo_url = process.env.MONGODB_URL;
 mongoose
   .connect(mongo_url)
@@ -27,140 +45,67 @@ mongoose
     console.log(err);
   });
 
-// HOME PAGE
+// Express middleware
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(allowCors);
+
+// Home route
 app.get("/", (req, res) => {
   res.send("HI");
 });
 
-// GET USER PAGE
+// Get user route
 app.post("/getuser", async (req, res) => {
   try {
-    const senduser = await CustomerModel.findOne({
-      _id: req.body.id,
-    });
-    if (senduser) {
-      res.send({ senduser });
-    } else {
-      res.send({ message: 0 });
-    }
+    // Your implementation
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
-// CUSTOMER LOGIN PAGE
+// Customer login route
 app.post("/customerlogin", async (req, res) => {
   try {
-    const checkCustomer = await CustomerModel.findOne({
-      username: req.body.username,
-      password: req.body.password,
-    });
-
-    if (checkCustomer) {
-      const token_id = checkCustomer._id;
-      jwt.sign(
-        { token_id },
-        secret,
-        {
-          expiresIn: "15d",
-        },
-        (err, token) => {
-          if (err) {
-            res.status(500).send({ error: "Internal server error" });
-          }
-          res.send({ token: token, error: false });
-        }
-      );
-    } else {
-      res.send({ error: true });
-    }
+    // Your implementation
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
-// CUSTOMER SIGN UP
+// Customer signup route
 app.post("/customersignup", async (req, res) => {
   try {
-    const verifyDuplicacy = await CustomerModel.findOne({
-      username: req.body.username,
-      email: req.body.email,
-      phoneno: req.body.phoneno,
-    });
-    if (verifyDuplicacy) {
-      res.send("User Already Exists !!!");
-    } else {
-      const insertCustomerData = await CustomerModel.create({
-        fname: req.body.fname,
-        lname: req.body.lname,
-        username: req.body.username,
-        phoneno: req.body.phoneno,
-        email: req.body.email,
-        password: req.body.password,
-      });
-      if (insertCustomerData) {
-        res.send({ message: "Successfully Inserted !!!" });
-      } else {
-        res.send({ message: "Unable to Insert Data !!!" });
-      }
-    }
+    // Your implementation
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
-// MANAGER LOGIN PAGE
+// Manager login route
 app.post("/managerlogin", async (req, res) => {
   try {
-    const checkManager = await ManagerModel.findOne({
-      mid: req.body.mid,
-      password: req.body.password,
-    });
-
-    if (checkManager) {
-      const token_id = checkManager._id;
-      jwt.sign(
-        { token_id },
-        secret,
-        {
-          expiresIn: "15d",
-        },
-        (err, token) => {
-          if (err) {
-            res.status(500).send({ error: "Internal server error" });
-          }
-          res.send({ token: token });
-        }
-      );
-    } else {
-      res.send({ error: 0 });
-    }
+    // Your implementation
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
-// GET PRODUCTS PAGE
+// Get products route
 app.post("/getproduct", async (req, res) => {
   try {
-    const products = await CustomerModel.find({});
-    if (products) {
-      res.send({ products });
-    } else {
-      res.send({ message: 0 });
-    }
+    // Your implementation
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
-// NEW PAGE
-
+// Listen on port 8080
 app.listen(8080, () => {
-  console.log("Connected to", PORT);
+  console.log("Connected to 8080");
 });
